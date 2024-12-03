9fea9dec14f8785b3e169940b54060fa4029ca3f
Gabriel Rosa da Silva <gabriel.rosa.080305@gmail.com>
2024-12-02 22:48:42 -0300

    Delete java_api directory
    No reason to keep a project that didn't continue (for now)

--- java_api/src/main/java/dnd_companion/java_api/dto/ArmorRegistryData.java
+++
@@ -1,9 +0,0 @@
-package dnd_companion.java_api.dto;
-
-public record ArmorRegistryData(
-	String name,
-	Object[] tags, 
-	double price, 
-	double weight, 
-	String description
-){}
--- java_api/src/main/java/dnd_companion/java_api/repositories/OptionRepository.java
+++
@@ -1,4 +0,0 @@
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-}
