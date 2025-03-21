3849251cd312be678c902b194b0003225b285e70
Gabriel Rosa <gabriel.rosa.080305@gmail.com>
2025-03-20 22:02:39 -0300

    Adding files that weren't uploaded

---
+++ src/main/java/GabrielRosa835/DnD_Companion/app/elements/entities/system/MeasureRepository.java
@@ -0,0 +1,5 @@
+package GabrielRosa835.dnd_companion.app.elements.entities.system;
+
+import org.springframework.data.jpa.repository.*;
+
+public interface MeasureRepository extends JpaRepository<Measure, Integer> {}
